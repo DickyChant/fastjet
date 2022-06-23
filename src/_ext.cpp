c60553e896f164b383e8d0b7343504b789d33bd2
Sitian Qian <stqian@pku.edu.cn>
2022-06-23 16:42:33 +0000

    add softdrop

--- src/_ext.cpp
+++ src/_ext.cpp
@@ -13,6 +13,9 @@
 #include <fastjet/AreaDefinition.hh>
 #include <fastjet/GhostedAreaSpec.hh>
 
+#include <fastjet/tools/Pruner.hh>
+#include <fastjet/contrib/SoftDrop.hh>
+
 #include <pybind11/pybind11.h>
 #include <pybind11/operators.h>
 #include <pybind11/numpy.h>
@@ -48,6 +51,7 @@ class output_wrapper{
     return a;
   }
   void setCluster(){}
+
 };
 
 output_wrapper interfacemulti(py::array_t<double, py::array::c_style | py::array::forcecast> pxi, py::array_t<double, py::array::c_style | py::array::forcecast> pyi, py::array_t<double, py::array::c_style | py::array::forcecast> pzi, py::array_t<double, py::array::c_style | py::array::forcecast> Ei,py::array_t<int, py::array::c_style | py::array::forcecast> offsets, py::object jetdef)
@@ -92,6 +96,7 @@ output_wrapper interfacemulti(py::array_t<double, py::array::c_style | py::array
   }
   return ow;
 }
+
 
 PYBIND11_MODULE(_ext, m) {
   using namespace fastjet;
@@ -152,6 +157,66 @@ PYBIND11_MODULE(_ext, m) {
             off
           );
       }, "min_pt"_a = 0, R"pbdoc(
+        Retrieves the inclusive jets from multievent clustering and converts them to numpy arrays.
+        Args:
+          min_pt: Minimum jet pt to include. Default: 0.
+        Returns:
+          pt, eta, phi, m of inclusive jets.
+      )pbdoc")
+      .def("to_numpy_softdrop",[](const output_wrapper ow,double min_pt = 0, double z_cut = 0, double beta = 0){
+        auto css = ow.cse;
+        int64_t len = css.size();
+        // Don't specify the size if using push_back.
+        auto jk = 0;
+        for(int i = 0; i < len; i++){
+        jk += css[i]->inclusive_jets(min_pt).size();
+        }
+        auto px = py::array(py::buffer_info(nullptr, sizeof(double), py::format_descriptor<double>::value, 1, {jk}, {sizeof(double)}));
+        auto bufpx = px.request();
+        double *ptrpx = (double *)bufpx.ptr;
+
+        auto py = py::array(py::buffer_info(nullptr, sizeof(double), py::format_descriptor<double>::value, 1, {jk}, {sizeof(double)}));
+        auto bufpy = py.request();
+        double *ptrpy = (double *)bufpy.ptr;
+
+        auto pz = py::array(py::buffer_info(nullptr, sizeof(double), py::format_descriptor<double>::value, 1, {jk}, {sizeof(double)}));
+        auto bufpz = pz.request();
+        double *ptrpz = (double *)bufpz.ptr;
+
+        auto E = py::array(py::buffer_info(nullptr, sizeof(double), py::format_descriptor<double>::value, 1, {jk}, {sizeof(double)}));
+        auto bufE = E.request();
+        double *ptrE = (double *)bufE.ptr;
+
+        auto off = py::array(py::buffer_info(nullptr, sizeof(int), py::format_descriptor<int>::value, 1, {len}, {sizeof(int)}));
+        auto bufoff = off.request();
+        int *ptroff = (int *)bufoff.ptr;
+        size_t idxe = 0;
+        *ptroff = 0;
+        ptroff++;
+        Pruner pruner(fastjet::cambridge_algorithm, 0.1, 0.5);
+        contrib::SoftDrop sd(beta, z_cut);
+        for(int i = 0; i < len; i++){
+        auto jets = ow.cse[i]->inclusive_jets(min_pt);
+        for (unsigned int j = 0; j < jets.size(); j++)
+        {
+          auto sd_jet = sd(jets[j]);
+          ptrpx[idxe] = sd_jet.px();
+          ptrpy[idxe] = sd_jet.py();
+          ptrpz[idxe] = sd_jet.pz();
+          ptrE[idxe] =  sd_jet.E();
+          idxe++;
+        }
+        *ptroff = jets.size()+*(ptroff-1);
+        ptroff++;
+        }
+        return std::make_tuple(
+            px,
+            py,
+            pz,
+            E,
+            off
+          );
+      },R"pbdoc(
         Retrieves the inclusive jets from multievent clustering and converts them to numpy arrays.
         Args:
           min_pt: Minimum jet pt to include. Default: 0.
@@ -1828,5 +1893,6 @@ PYBIND11_MODULE(_ext, m) {
       )pbdoc");
   py::class_<ClusterSequence>(m, "ClusterSequence")
     .def(py::init<const std::vector<PseudoJet> &, const JetDefinition &, const bool &>(), "pseudojets"_a, "jet_definition"_a, "write_out_combinations"_a = false, "Create a ClusterSequence, starting from the supplied set of PseudoJets and clustering them with jet definition specified by jet_definition (which also specifies the clustering strategy)");
+
 
 }
